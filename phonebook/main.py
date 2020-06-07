fbf76d023aa8affb7a4fb8f64d3742707a611e88
Rinlapat <rlt.silverice@gmail.com>
2020-06-07 23:22:11 +0700

    refactor code

--- phonebook/main.py
+++ phonebook/main.py
@@ -2,10 +2,10 @@ import re
 
 
 def phone(phonebook, phone_no):
-
-    if phonebook.count(phone_no) > 1:
+    count_occurs = phonebook.count(phone_no)
+    if count_occurs > 1:
         return "Error => Too many people: {}".format(phone_no)
-    elif phonebook.count(phone_no) < 1:
+    elif count_occurs < 1:
         return "Error => Not found: {}".format(phone_no)
 
     name = ""
@@ -13,9 +13,11 @@ def phone(phonebook, phone_no):
     pb_arr = phonebook.split("\n")
     for record in pb_arr:
         if record.find(phone_no) > 0:
+            # find name
             name = record[record.find("<") + 1:record.find(">")]
+            # find address
             mess_address = record.replace(phone_no, "").replace(name, "")
-            clean_address = re.sub(r"[()\"#/@;:<>{}`+=~|!?,$]", "", mess_address).strip()
+            clean_address = "".join(ch for ch in mess_address if ch.isalnum() or ch in [".", "-", " ", "_"]).strip()
             address = re.sub("( +)|_", " ", clean_address)
 
     return "Phone => {num}, Name => {name}, Address => {address}".format(num=phone_no, name=name, address=address)
